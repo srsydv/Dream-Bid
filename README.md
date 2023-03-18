251a7ed083972c89c390ba5a96847e61ecbe02a8
srsydv <shrish@iiitmanipur.ac.in>
2023-03-18 17:19:26 +0530

    add Bidders

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Dream-Bid
+
+---Bid system
+1.there will be some fields like 
+--open for everyone
+--you can select the number of people for your own bid game and add them or you can open it for request from other people (protocol will get the percentage only)
+1.1--who will win the election
+suppose IND and PAK
+1.1.1---people will bid with there ERC20 for one and suppose result will come on 1 jan 2024 and bid started on 1 Dec 2023
+1.1.2---After 1 jan 2024 the result will come out we will take vote from those bidders till 7 days(It's not fixed yet) for withdrawing the ERC20
+1.1.3---who will vote will get there ERC20 along with percentage
+1.1.4---Percantage
+1.1.4.1---suppose x votes for IND and while voting we will take a input of percantage like how much benifite they want
+1.1.4.1---Important:::suppose IND loses the match the fund will bd diducted by that percantage (ex: x supplied 100 usdc with 10% interest rate after loosing the match 10usdc will be diducted and he can
+	 withdraw his 90usdc or keep it into contract for next BID game)
+1.1.4.2---If IND wins the game then x will gate the amount of total percentage
---
+++ bidRegistry.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract bidRegistry is ReentrancyGuard {
+    using Counters for Counters.Counter;
+
+    uint256 public gameId;
+
+    enum gameState {
+        SCEDULED,
+        STARTED,
+        RUNNING,
+        FINISHED
+    }
+
+    struct gameDetail {
+        address ERC20contract;
+        address gameOwner;
+        uint256 gameCreationTime;
+        uint256 price;
+        bool fixPrice;
+        bool addBidders;
+        uint256 bidStartTime;
+        uint256 bidEndTime;
+        gameState state;
+    }
+
+    // 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+    // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+    // 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+    // 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+
+    // gameId => gameDetail
+    mapping(uint256 => gameDetail) public gamesDetail;
+    // gameId => Bidders Address
+    mapping(uint256 => address[]) public Bidders;
+    // tokenId => (Bidder Address => Bidder Address index)
+    mapping(uint256 => mapping(address => uint256)) public bidderAddressIndex;
+
+    event gameListedForFixPrice(
+        address ERC20contract,
+        uint256 gameId,
+        uint256 price,
+        uint256 bidStartTime,
+        uint256 bidEndTime
+    );
+
+    event BidderAdded(uint256 gameId, address BidderAddress);
+    event BidderRemoved(uint256 gameId, address BidderAddress);
+
+    modifier onlyGameOwner(uint256 _gameId) {
+        gameDetail storage game = gamesDetail[_gameId];
+        require(msg.sender == game.gameOwner, "You are not the game owner");
+        _;
+    }
+
+    function listGame(
+        address _ERC20contract,
+        uint256 _price,
+        uint256 _bidStartTime,
+        uint256 _bidEndTime,
+        bool _addBidders
+    ) external nonReentrant {
+        require(
+            _ERC20contract != address(0),
+            "you can't do this with zero address"
+        );
+        uint256 gameId_ = ++gameId;
+        gameDetail memory gameData = gameDetail(
+            _ERC20contract,
+            msg.sender,
+            block.timestamp,
+            _price,
+            true,
+            _addBidders,
+            block.timestamp + _bidStartTime,
+            block.timestamp + _bidEndTime,
+            gameState.STARTED
+        );
+        gamesDetail[gameId_] = gameData;
+
+        emit gameListedForFixPrice(
+            _ERC20contract,
+            gameId_,
+            _price,
+            block.timestamp + _bidStartTime,
+            block.timestamp + _bidEndTime
+        );
+    }
+
+    function addBidders(uint256 _gameId, address _bidderAddress)
+        external
+        nonReentrant
+    {
+        require(_bidderAddress != address(0), "0 address given");
+        gameDetail storage game = gamesDetail[_gameId];
+        require(game.gameOwner == msg.sender, "You are not owner");
+        require(block.timestamp < game.bidStartTime, "Game has been started");
+        require(game.addBidders, "You can't add Bidder");
+        _addBidder(_gameId, _bidderAddress);
+        emit BidderAdded(_gameId, _bidderAddress);
+    }
+
+    function _addBidder(uint256 _gameId, address _bidderAddress) private {
+        bidderAddressIndex[_gameId][_bidderAddress] = Bidders[_gameId].length;
+        Bidders[_gameId].push(_bidderAddress);
+    }
+
+    function removeBidder(uint256 _gameId, address _bidderAddress)
+        external
+        nonReentrant
+    {
+        require(_bidderAddress != address(0), "0 address given");
+        gameDetail storage game = gamesDetail[_gameId];
+        require(game.gameOwner == msg.sender, "You are not owner");
+        require(block.timestamp < game.bidStartTime, "Game has been started");
+        require(game.addBidders, "You can't add Bidder");
+        _removeBidder(_gameId, _bidderAddress);
+        emit BidderRemoved(_gameId, _bidderAddress);
+    }
+
+    function _removeBidder(uint256 _gameId, address _bidderAddress) private {
+        uint256 lastBidderIndex = Bidders[_gameId].length - 1;
+        address lastBidderAddress = Bidders[_gameId][lastBidderIndex];
+        if (_bidderAddress != lastBidderAddress) {
+            uint256 bidderIndex = bidderAddressIndex[_gameId][_bidderAddress];
+            Bidders[_gameId][bidderIndex] = lastBidderAddress;
+            bidderAddressIndex[_gameId][lastBidderAddress] = bidderIndex;
+        }
+        delete bidderAddressIndex[_gameId][_bidderAddress];
+        Bidders[_gameId].pop();
+    }
+}
