9ffaccfe6dc63255cdfabd172ce5aac2759d699f
srsydv <shrish@iiitmanipur.ac.in>
2023-05-02 11:29:02 +0530

    Bid

--- test/test.gameRegistry.js
+++ test/test.gameRegistry.js
@@ -19,6 +19,12 @@ contract("gameRegistry", async (accounts) => {
     let Bidder1 = accounts[1];
     let Bidder2 = accounts[2];
     let Bidder3 = accounts[3];
+    let Bidder4 = accounts[4];
+    let Bidder5 = accounts[5];
+    let Bidder6 = accounts[6];
+    let Bidder7 = accounts[7];
+    let Bidder8 = accounts[8];
+    let Bidder9 = accounts[9];
 
     bidStartTime = BigNumber(moment.now());
     bidEndTime =  BigNumber(moment.now()).plus(200);
@@ -116,22 +122,65 @@ contract("gameRegistry", async (accounts) => {
         res = await GameRegistry.addBidders(1, Bidder1);
         res = await GameRegistry.addBidders(1, Bidder2);
         res = await GameRegistry.addBidders(1, Bidder3);
+        res = await GameRegistry.addBidders(1, Bidder4);
+        res = await GameRegistry.addBidders(1, Bidder5);
+        res = await GameRegistry.addBidders(1, Bidder6);
+        res = await GameRegistry.addBidders(1, Bidder7);
+        res = await GameRegistry.addBidders(1, Bidder8);
+        res = await GameRegistry.addBidders(1, Bidder9);
+        
     })
 
-    it("let game owner remove the Bidder3", async() => {
-        await GameRegistry.removeBidder(1,Bidder3)
-    })
 
     it("let Bidders Bid on game1", async() => {
         await ERC20.mint(Bidder1, '10000000000');
+        await ERC20.mint(Bidder2, '10000000000');
+        await ERC20.mint(Bidder3, '10000000000');
+        await ERC20.mint(Bidder4, '10000000000');
+        await ERC20.mint(Bidder5, '10000000000');
+        await ERC20.mint(Bidder6, '10000000000');
+        await ERC20.mint(Bidder7, '10000000000');
+        await ERC20.mint(Bidder8, '10000000000');
+        await ERC20.mint(Bidder9, '10000000000');
         let balance = await ERC20.balanceOf(Bidder1);
         console.log("Bidder1 Balance",balance.toString())
         console.log("times",bidStartTime.toString(), "Hi", BigNumber(moment.now()).toString())
         await ERC20.approve(BidGame.address, 10000, { from: Bidder1 })
-        let x = await ERC20.allowance(BidGame.address, Bidder1)
-        // console.log("hj111j",x.toString())
-        await BidGame.Bid(2,0,5000, {from: Bidder1})
         await BidGame.Bid(1,0,5000, {from: Bidder1})
+
+        await ERC20.approve(BidGame.address, 50000, { from: Bidder2 })
+        await BidGame.Bid(1,0,50000, {from: Bidder2})
+
+        await ERC20.approve(BidGame.address, 150000, { from: Bidder3 })
+        await BidGame.Bid(1,0,15000, {from: Bidder3})
+
+        await ERC20.approve(BidGame.address, 55000, { from: Bidder4 })
+        await BidGame.Bid(1,1,55000, {from: Bidder4})
+
+        await ERC20.approve(BidGame.address, 25000, { from: Bidder5 })
+        await BidGame.Bid(1,1,25000, {from: Bidder5})
+
+        await ERC20.approve(BidGame.address, 45000, { from: Bidder6 })
+        await BidGame.Bid(1,1,45000, {from: Bidder6})
+
+        await ERC20.approve(BidGame.address, 65000, { from: Bidder7 })
+        await BidGame.Bid(1,2,65000, {from: Bidder7})
+
+        await ERC20.approve(BidGame.address, 45000, { from: Bidder8 })
+        await BidGame.Bid(1,2,45000, {from: Bidder8})
+
+        await ERC20.approve(BidGame.address, 35000, { from: Bidder9 })
+        await BidGame.Bid(1,2,35000, {from: Bidder9})
+    })
+
+
+    it("let anyone BID on game2", async() => {
+        await ERC20.approve(BidGame.address, 10000, { from: Bidder1 })
+        await BidGame.Bid(2,0,5000, {from: Bidder1})
+    })
+
+    it("let game owner remove the Bidder3", async() => {
+        await GameRegistry.removeBidder(1,Bidder3)
     })
 
     it("let Bidder3 Bid on game1", async() => {
